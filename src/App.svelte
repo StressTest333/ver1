27a22f28f394b5fe41574e2cd5c64071f338b104
StressTest333 sid <yourshadow367@gmail.com>
2024-10-09 06:51:47 +0000

    ver1/1

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,17 @@
 	import Nested from './text.svelte';
   let string = `this string contains some <strong>HTML!!!</strong>`;
   let count = 0;
+  let numbers = [1, 2];
+  $: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
   $: doubled = count * 5;
   $: if (count >= 1000) {
 		alert('Достигнут лимит значений, счётчик обнуляется!');
 		count = 0;
+    numbers = [1, 2];
 	}
 	function increment() {
 		count += 100;
+    numbers = [...numbers, numbers.length + 1];
 	}
   
 </script>
@@ -21,12 +25,12 @@
   <h3>Начало {Title}!</h3>
   <img {src} alt="{Title} dances." />
   <p>Котяра</p>
-  
   <button on:click={increment}>
     Счётчик, {count}
     {count === 1 ? 'баллов' : 'баллов'}
   </button>
   <p>{count} * 5 = {doubled}</p>
+  <p>{numbers.join(' + ')} = {sum}</p>
   <Nested />
   <p>{@html string}</p>
 </main>
