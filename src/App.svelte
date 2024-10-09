a163c69e720d88f418dac71d4bb700460ae20da5
StressTest333 sid <yourshadow367@gmail.com>
2024-10-09 06:27:46 +0000

    new

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,25 @@
+
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let Title = 'обучения';
+  let src = 'https://www.meme-arsenal.com/memes/f5f8c0a96659af21ee2a18086ae8cd3a.jpg';
+	let name = 'Rick Astley';
+  import Nested from './text.svelte';
+  let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h2>Добро пожаловать!</h2>
+  <h3>Начало {Title}!</h3>
+  <img {src} alt="{name} dances." />
+  <p>Котяра</p>
+  <Nested />
+  <p>{@html string}</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	p {
+		color: green;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
 </style>
