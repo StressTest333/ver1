2dbddb86ff5aa0661a6c538a12b0ef449d1e447e
StressTest333 sid <yourshadow367@gmail.com>
2024-10-09 06:44:49 +0000

    ver1

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,31 @@
 <script>
   let Title = 'обучения';
   let src = 'https://www.meme-arsenal.com/memes/f5f8c0a96659af21ee2a18086ae8cd3a.jpg';
-	let name = 'Rick Astley';
-  import Nested from './text.svelte';
+	import Nested from './text.svelte';
   let string = `this string contains some <strong>HTML!!!</strong>`;
+  let count = 0;
+  $: doubled = count * 5;
+  $: if (count >= 1000) {
+		alert('Достигнут лимит значений, счётчик обнуляется!');
+		count = 0;
+	}
+	function increment() {
+		count += 100;
+	}
+  
 </script>
 
 <main>
   <h2>Добро пожаловать!</h2>
   <h3>Начало {Title}!</h3>
-  <img {src} alt="{name} dances." />
+  <img {src} alt="{Title} dances." />
   <p>Котяра</p>
+  
+  <button on:click={increment}>
+    Счётчик, {count}
+    {count === 1 ? 'баллов' : 'баллов'}
+  </button>
+  <p>{count} * 5 = {doubled}</p>
   <Nested />
   <p>{@html string}</p>
 </main>
